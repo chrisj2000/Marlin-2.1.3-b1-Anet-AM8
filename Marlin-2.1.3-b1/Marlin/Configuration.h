ace28cc3c22350eecf800f22e4a3bf47a706bf91
chrisj2000 <42233475+chrisj2000@users.noreply.github.com>
2025-02-22 17:04:47 +0800

    update board

--- Marlin-2.1.3-b1/Marlin/Configuration.h
+++ Marlin-2.1.3-b1/Marlin/Configuration.h
@@ -68,7 +68,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_MKS_GEN_L
+  #define MOTHERBOARD BOARD_MKS_GEN_L_V21
 #endif
 
 // @section serial
